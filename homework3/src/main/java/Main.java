11aa15209abc9a5cd87d9a19845de42396bef067
Sergio600 <sergio600@mail.ru>
2021-01-27 13:02:52 +0300

    hw3
    get json and parse it.

--- homework3/src/main/java/Main.java
+++ homework3/src/main/java/Main.java
@@ -1,26 +1,80 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.HttpURLConnection;
-import java.net.URLConnection;
+
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String url  = "https://jsonplaceholder.typicode.com/posts";
-        URL myURL = new URL(url);
-
-        URLConnection connection = myURL.openConnection();
-
-        System.out.println("Тип содержимого:" + connection.getContentType());
-       ;
-
-        System.out.println(connection.getContent().toString());
+        String url  = "https://jsonplaceholder.typicode.com/comments";
+        String json = getJSON(url);
+        System.out.println(json);
 
 
+//        JSONObject obj = new JSONObject(json);
+//
+//        JSONArray arr= obj.getJSONArray("");
+//        String result = arr.getJSONObject(0).getString("id");
+//        System.out.println(result);
+
+
+        JSONArray arr = new JSONArray(json);
+        String s = arr.getJSONObject(0).toString();
+
+        JSONObject jsonObject= arr.getJSONObject(0);
 
 
 
+        System.out.println(jsonObject.getString("email"));
+        System.out.println(jsonObject.length());
+        System.out.println(jsonObject.getInt("id"));
+
+
+
+
+
+
+
+    }
+
+    public static String getJSON(String url) {
+        HttpsURLConnection con = null;
+        try {
+            URL u = new URL(url);
+            con = (HttpsURLConnection) u.openConnection();
+
+            con.connect();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            br.close();
+            return sb.toString();
+
+
+        } catch (MalformedURLException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (con != null) {
+                try {
+                    con.disconnect();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        return null;
     }
 }
